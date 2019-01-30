3d04f3685fb12c1e0437eb518fccda7e28984259
Paris Xavier Pinkney <hello@pxpgraphics.com>
2019-01-30 15:37:48 -0800

    Initial commit.

---
+++ Source/Badger.h
@@ -0,0 +1,19 @@
+//
+//  Badger.h
+//  Badger
+//
+//  Created by Paris Pinkney on 1/30/19.
+//  Copyright © 2019 PXPGraphics, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Badger.
+FOUNDATION_EXPORT double BadgerVersionNumber;
+
+//! Project version string for Badger.
+FOUNDATION_EXPORT const unsigned char BadgerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Badger/PublicHeader.h>
+
+
---
+++ Tests/BaseTestCase.swift
@@ -0,0 +1,11 @@
+//
+//  BaseTestCase.swift
+//  Badger-iOS-Tests
+//
+//  Created by Paris Pinkney on 1/30/19.
+//  Copyright © 2019 PXPGraphics, Inc. All rights reserved.
+//
+
+import Badger
+import Foundation
+import XCTest
